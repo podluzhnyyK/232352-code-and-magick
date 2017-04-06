ba6dc70f2bb10624227dd613fc834324ffc600d4
podluzhyyK <podluzhnyy94@mail.ru>
2017-04-06 08:05:51 +0400

    Добавил функции, проходит тест

--- js/stat.js
+++ js/stat.js
@@ -1,19 +1,20 @@
 'use strict';
 window.renderStatistics = function (ctx, names, times) {
- function getBackground(){
+  function getBackground() {
     ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.strokeRect(110, 20, 270, 420);
-  ctx.fillRect(110, 20, 270, 420);
-  ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
-  ctx.strokeRect(100, 10, 270, 420);
-  ctx.fillRect(100, 10, 270, 420);
-  };
+    ctx.strokeRect(110, 20, 270, 420);
+    ctx.fillRect(110, 20, 270, 420);
+    ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
+    ctx.strokeRect(100, 10, 270, 420);
+    ctx.fillRect(100, 10, 270, 420);
+  }
   getBackground();
-  function getTextBackground(){
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', 120, 40);
-  ctx.fillText('Список результатов:', 120, 60);
+
+  function getTextBackground() {
+    ctx.fillStyle = '#000';
+    ctx.font = '16px PT Mono';
+    ctx.fillText('Ура вы победили!', 120, 40);
+    ctx.fillText('Список результатов:', 120, 60);
   }
   getTextBackground();
   var max = -1;
