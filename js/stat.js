d63e458143920afb99dea7056ecd836e5be325dd
podluzhyyK <podluzhnyy94@mail.ru>
2017-04-09 15:11:09 +0400

    Изменил функции

--- js/stat.js
+++ js/stat.js
@@ -1,22 +1,27 @@
 'use strict';
 window.renderStatistics = function (ctx, names, times) {
-  function getBackground() {
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-    ctx.strokeRect(110, 20, 270, 420);
-    ctx.fillRect(110, 20, 270, 420);
-    ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
-    ctx.strokeRect(100, 10, 270, 420);
-    ctx.fillRect(100, 10, 270, 420);
-  }
-  getBackground();
 
-  function getTextBackground() {
-    ctx.fillStyle = '#000';
-    ctx.font = '16px PT Mono';
-    ctx.fillText('Ура вы победили!', 120, 40);
-    ctx.fillText('Список результатов:', 120, 60);
-  }
-  getTextBackground();
+  var histogramWidth = 150;
+  var stepCoordinate = 120;
+  var currentColor = '#000000';
+
+  var drawRect = function (x1, y1, x2, y2, color) {
+    ctx.fillStyle = color || '#000000';
+    ctx.fillRect(x1, y1, x2, y2);
+  };
+
+  var drawText = function (text, x, y, color, font) {
+    ctx.fillStyle = color || '#000';
+    ctx.font = font || '16px PT Mono';
+    ctx.fillText(text, x, y);
+  };
+
+  drawRect(110, 20, 270, 420, 'rgba(0, 0, 0, 0.7)');
+  drawRect(100, 10, 270, 420, 'rgba(256, 256, 256, 1.0)');
+
+  drawText('Ура вы победили!', 120, 40);
+  drawText('Список результатов:', 120, 60);
+
   var max = -1;
   var i;
   for (i = 0; i < times.length; i++) {
@@ -26,22 +31,24 @@ window.renderStatistics = function (ctx, names, times) {
     }
   }
 
-  function getRandomOpacity(min, max2) {
+  var getRandomOpacity = function (min, max2) {
     return Math.random() * (max2 - min) + min;
-  }
-  var histogramWidth = 150;
+  };
+
   var step = histogramWidth / (-max - 0);
-  var stepCoordinate = 120;
 
   for (i = 0; i < times.length; i++) {
     if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+      currentColor = 'rgba(255, 0, 0, 1)';
     } else {
-      ctx.fillStyle = 'rgba(0, 21, 255,' + getRandomOpacity(0.1, 1) + ')';
+      currentColor = 'rgba(0, 21, 255,' + getRandomOpacity(0.1, 1) + ')';
     }
-    ctx.fillText(Math.round(times[i]), stepCoordinate, 240 + times[i] * step);
-    ctx.fillText(names[i], stepCoordinate, 280);
-    ctx.fillRect(stepCoordinate, 260, 40, times[i] * step);
+
+    drawText(Math.round(times[i]), stepCoordinate, 240 + times[i] * step);
+    drawText(names[i], stepCoordinate, 280);
+
+    drawRect(stepCoordinate, 260, 40, times[i] * step, currentColor);
+
     stepCoordinate += 50;
   }
 
